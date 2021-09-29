bdab8d05ffa3a43544edccd7a51470ac89dc2d3c
Lior Polischouk <67924209+Lior4224@users.noreply.github.com>
2021-09-29 12:25:21 -0400

    Create Greet.java

---
+++ 07_helloclass/Greet.java
@@ -0,0 +1,11 @@
+/*
+Team with a vowel (Lior, Alif, Josiah)
+APCS HW#7 - helloclass
+9/29/21
+*/
+
+public class Greet {
+  public static void main (String[] args) {
+    System.out.println("No hablo queso.");
+  }
+}
