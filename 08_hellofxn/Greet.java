b7141f6bf520ff3b48242fe874843ea0ad3cd7dc
Lior Polischouk <67924209+Lior4224@users.noreply.github.com>
2021-09-30 18:42:03 -0400

    Update Greet.java

--- 08_hellofxn/Greet.java
+++ 08_hellofxn/Greet.java
@@ -2,7 +2,7 @@ public class Greet {
   public static void main (String [] args) {
     greet();
   }
-  public static void greet (String [] args) {
+  public static void greet () {
     System.out.println("Hi, Daniel. How are you?");
     System.out.println("Hello, Jeff. I haven't seen you in a while!");
     System.out.println("Greetings, Bob. Nice to meet you.");
