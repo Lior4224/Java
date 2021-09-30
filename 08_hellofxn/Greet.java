f98a31ec81ddbc4d178638e5f093075f5a54fcfe
Lior Polischouk <67924209+Lior4224@users.noreply.github.com>
2021-09-30 18:47:45 -0400

    Update Greet.java

--- 08_hellofxn/Greet.java
+++ 08_hellofxn/Greet.java
@@ -1,3 +1,9 @@
+/*
+Lior Polischouk
+APCS
+HW#08: Refactor Freshie Zero
+9/30/21
+*/
 public class Greet {
   public static void main (String [] args) {
     greet();
@@ -8,3 +14,10 @@ public class Greet {
     System.out.println("Greetings, Bob. Nice to meet you.");
   }
 }
+/*
+DISCOVERIES:
+-Noticed that doing greet (String [] args) instead of just greet () didn't work, since I had no args to
+put into greet() when I called it in main.
+UNRESOLVED QUESTIONS:
+-none
+*/
